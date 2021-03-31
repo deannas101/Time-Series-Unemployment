a1130df57e4f4d5a0c514d9a7123eb8ff7366ec1
deannas101 <52871125+deannas101@users.noreply.github.com>
2021-03-31 14:16:51 -0400

    Update Houghton_Unemployment.R

--- Houghton_Unemployment.R
+++ Houghton_Unemployment.R
@@ -2,3 +2,15 @@
 MIHOUG1URN <- read_csv("MIHOUG1URN.csv", 
                        col_types = cols(DATE = col_date(format = "%Y-%m-%d")))
 
+# Preliminary Plotting --------------------------------------------------------
+
+
+# Deterministic Trend: Assumption Checking ------------------------------------
+
+
+# Transformation if necessary -------------------------------------------------
+
+
+# Select candidate for p,q,d --------------------------------------------------
+
+
