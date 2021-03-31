c6c2c43f2e70c6ca3de479a41eb3e576a5fd23db
deannas101 <52871125+deannas101@users.noreply.github.com>
2021-03-31 14:09:34 -0400

    Update Houghton_Unemployment.R

--- Houghton_Unemployment.R
+++ Houghton_Unemployment.R
@@ -1,3 +1,4 @@
 # Data Importing from CSV -----------------------------------------------------
 MIHOUG1URN <- read_csv("MIHOUG1URN.csv", 
                        col_types = cols(DATE = col_date(format = "%Y-%m-%d")))
+
