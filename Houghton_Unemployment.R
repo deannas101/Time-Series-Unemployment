380cf8ee4e36b390c524d2c90c78193bf93ff817
deannas101 <52871125+deannas101@users.noreply.github.com>
2021-03-31 14:25:18 -0400

    Update Houghton_Unemployment.R

--- Houghton_Unemployment.R
+++ Houghton_Unemployment.R
@@ -1,5 +1,5 @@
 # Data Importing from CSV -----------------------------------------------------
-MIHOUG1URN <- read_csv("MIHOUG1URN.csv", 
+unemployment <- read_csv("~/GitHub/Time-Series-Unemployment/MIHOUG1URN.csv", 
                        col_types = cols(DATE = col_date(format = "%Y-%m-%d")))
 
 # Preliminary Plotting --------------------------------------------------------
